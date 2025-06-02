d5ccb9bfb19e228bbc6eceafa1ddc00dd21ad445
miguel <miguel.scaccia1@gmail.com>
2025-06-01 21:32:27 -0300

    Se agrega la parte final del modulo memoria.sh

--- memoria.sh
+++ memoria.sh
@@ -16,4 +16,23 @@ while true; do
             echo ">>> Uso de memoria RAM y SWAP:"
             free -h
             ;;
-
+        2)
+            echo
+            echo ">>> Contenido de /proc/meminfo (primeras 20 líneas):"
+            head -n 20 /proc/meminfo
+            ;;
+        3)
+            echo
+            echo ">>> Salida de vmstat:"
+            vmstat
+            ;;
+        0)
+            break
+            ;;
+        *)
+            echo "Opción inválida. Intente nuevamente."
+            ;;
+    esac
+    echo
+    read -p "Presione Enter para continuar..."
+done
